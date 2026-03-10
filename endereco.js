8acb10e170408198932c39948722110d0e5f69e9
CadueFrank <carlos.machado14@escola.pr.gov.br>
2026-03-10 11:43:31 -0300

    Add files via upload

---
+++ endereco.js
@@ -0,0 +1,8 @@
+console.log("Endereço Completo");
+let rua = "rua das flores";
+let numero = 208;
+let cidade = "Curitiba";
+
+let endereco = rua + ", " + numero + " - " + cidade;
+//console.log(endereco);
+console.log("O endereço correto é: " + endereco)
