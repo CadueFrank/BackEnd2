131de5dbebf5e88a4ac46594fbae07cd11fa5d4b
CadueFrank <carlos.machado14@escola.pr.gov.br>
2026-03-24 14:43:41 +0000

    versao1

---
+++ interpolacao.js
@@ -0,0 +1,9 @@
+const nome = Ana;
+const idade = 27;
+const anoAtual = 2026;
+
+const anoNascimento = anoAtual - idade;
+const idadeEmDezAnos = idade + 10:
+console.log("O ano de nascimento é: " + anoNascimento);
+console.log(`${nome} possui ${idade} ano.`);
+console.log(`Daqui a dez anos, tera ${anoNascimento}`);
