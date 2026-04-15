36a040d4bb4bbae63a7eaad734de106c8617169d
CadueFrank <carlos.machado14@escola.pr.gov.br>
2026-04-15 13:46:37 +0000

    versao1

---
+++ lista.js
@@ -0,0 +1,12 @@
+const alunos = ["Arthur", "Kimberly", "Rubens"];
+
+alunos.push("Samuel");
+alunos.unshift("Marcio");
+console.log(alunos);
+
+console.log(alunos.indexOf("Samuel"));
+console.log(alunos.indexOf("Cadu"));
+
+console.log(alunos.includes("Cadu"));
+console.log(alunos.includes("Rubens"));
+
